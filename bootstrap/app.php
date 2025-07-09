b87f6545cea6db0acf03498c88387507548b5f4b
xuan-pham-1205 <xuan@z-gamestudio.com>
2025-07-09 11:35:57 +0700

    Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('purchase/step-two-submit') && $request->method() === 'GET') {
+                return response()->view('errors.custom_error', [], 200);
+            }
+
+            return null;
+        });
     })->create();
---
+++ resources/views/errors/custom_error.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<main>
+  <div class="container text-center py-5">
+    <h2 class="text-danger mb-4">【ご登録完了後に表示されるエラーメッセージについて】</h2>
+    <p>現在、一部の環境において会員登録手続き完了後にエラーメッセージが表示される不具合を確認しております。</p>
+    <p>ただし、お客様の登録データは問題なく正常に保存されておりますので、再度ご登録いただく必要はございません。</p>
+  </div>
+</main>
+@endsection
